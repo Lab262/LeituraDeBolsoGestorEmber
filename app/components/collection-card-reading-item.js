e038c7041f18b85dff30cdea5fa9ae1db44a64f9
thiagotmb <tmb0710@gmail.com>
2016-10-19 03:45:59 -0200

    Finish handle actions in card

--- app/components/collection-card-reading-item.js
+++ app/components/collection-card-reading-item.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  reading : {'Title': 'agora foi', 'Author': 'Nois', 'Content' : 'Agora foi uai carai'},
+
+  actions: {
+    debug: function(reading) {
+
+      console.log(reading);
+      console.log('vai');
+
+    },
+
+    toggleBody: function(reading) {
+      console.log(reading);
+      console.log('delete');
+    },
+
+    changeReadingOfTheWeek: function(isChecke) {
+      console.log(this.reading);
+      console.log('foi');
+    }
+  }
+
 });
