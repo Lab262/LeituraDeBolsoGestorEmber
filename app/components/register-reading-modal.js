b24efefa5e2037ed7bf182861da92294f112f3a4
thiagotmb <tmb0710@gmail.com>
2016-10-23 15:15:53 -0200

    Adding reading creation

--- app/components/register-reading-modal.js
+++ app/components/register-reading-modal.js
@@ -1,20 +1,21 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 
 export default Ember.Component.extend({
 
   readingTypes: ["Leitura lenta (100 palavras/minuto)", "Leitura média (130 palavras/minuto)", "Leitura rápida (160 palavras/minuto)"],
+  errors: [],
+  store: Ember.inject.service(),
 
-  readingTime: function() {
+  readingTimeInMinutes: Ember.computed('readingTypes','readingSelectedType','content', function() {
     let readingTypes = this.get('readingTypes')
     let readingSelectedType = (this.get('readingSelectedType') != null ? this.get('readingSelectedType') : "Leitura lenta (100 palavras/minuto)")
     let readingContent = (this.get('content') != null ? this.get('content') : " ")
-
     let numberOfWordsInReading = this.countWords(readingContent)
-
 
     switch(readingTypes.indexOf(readingSelectedType)) {
       case 0:
-        return this.calcReadingTime(100,numberOfWordsInReading)
+      return this.calcReadingTime(100,numberOfWordsInReading)
 
       case 1:
       return this.calcReadingTime(130,numberOfWordsInReading)
@@ -22,17 +23,7 @@ export default Ember.Component.extend({
       case 2:
       return this.calcReadingTime(160,numberOfWordsInReading)
     }
-  }.property('readingTypes','readingSelectedType','content'),
-
-  closeModalAction() {
-
-    var closeModalAction = this.get('closeModalAction');
-    if (closeModalAction) {
-      closeModalAction();
-    }
-  },
-
-  registerNewReading() { console.log("registernew")},
+  }),
 
   countWords(string){
     string = string.replace(/(^\s*)|(\s*$)/gi,"");//exclude  start and end white-space
@@ -42,23 +33,74 @@ export default Ember.Component.extend({
   },
 
   calcReadingTime(wordPerMinute, numberOfWordsInReading) {
-    return Math.round( (numberOfWordsInReading * 60) / wordPerMinute );
+    let readingTime = Math.round( ( (numberOfWordsInReading * 60) / wordPerMinute ) / 60 );
+
+    if (readingTime == 0) {
+
+      readingTime = 1;
+    }
+
+    return readingTime;
+  },
+
+  clearForm() {
+    this.set('title',"");
+    this.set('authorName',"");
+    this.set('content',"");
+    this.set('readOfTheDay',false);
+    this.set('readingTimeInMinutes', 1);
+
+  },
+
+  validateField(object, value) {
+    var errors = this.get('errors');
+
+    if (object == null || !object.trim()) {
+      errors.set(value,['Campo necessário']);
+      alert(value + ' é um campo obrigatório')
+      return false;
+    } else {
+      errors.set(value,null);
+      return true;
+    }
   },
 
   actions: {
 
-    changeReadingType() {
-      console.log("changed")
-    },
-
     closeModal() {
+      this.clearForm()
       this.closeModalAction()
 
     },
 
     submitForm() {
 
-      this.registerNewReading()
+      let title = this.get('title');
+      let authorName = this.get('authorName');
+      let content = this.get('content');
+      let readOfTheDay = this.get('readOfTheDay');
+      let timeToReadInMinutes = this.get('readingTimeInMinutes');
+
+      var validFieldsCount = 0;
+      validFieldsCount += this.validateField(title,'Título');
+      validFieldsCount += this.validateField(authorName,'Autor');
+      validFieldsCount += this.validateField(content,'Conteúdo');
+
+      if (validFieldsCount < 3) {
+        return;
+      }
+
+      var newReading = this.get('store').createRecord('reading', {
+       title: title,
+       authorName: authorName,
+       content: content,
+       readOfTheDay: readOfTheDay,
+       timeToReadInMinutes: timeToReadInMinutes
+     });
+
+     newReading.save();
+
+      this.clearForm()
       this.closeModalAction()
     }
 
